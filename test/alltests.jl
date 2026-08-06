28fbf5cca4181d06b2b51739cf37c1c5fbf56e7c
Jürgen Fuhrmann <juergen-fuhrmann@web.de>
2026-08-06 16:10:51 +0200

    Check debylength directly in runtests

--- test/alltests.jl
+++ test/alltests.jl
@@ -23,8 +23,8 @@ thisproject = dirname(Base.active_project())
 @testset "electrolytedata" begin
     ely = ElectrolyteData(c_bulk = fill(0.01 * mol / dm^3, 2))
     @test dlcap0(ely) ≈ 0.22846691848825248
-    ely = ElectrolyteData(c_bulk = fill(1 * mol / dm^3, 2))
-    @test conductivity(ely, ely.c_bulk) ≈ 15.02150977814831
+    @test debyelength(ely) ≈ 3.0418635924413116e-9
+    @test conductivity(ely, ely.c_bulk) ≈ 0.15021509778148306
 end
 
 @testset "doctests" begin
